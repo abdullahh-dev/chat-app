a71ba1b9e9e99824d0f39ade08af01c3817e1246
abdullahh-dev <abdullahaha101@example.com>
2025-02-19 11:24:08 +0500

    added form config for auth

--- app/components/Auth/AuthForm.tsx
+++ app/components/Auth/AuthForm.tsx
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+import { FORM_TYPE } from "./types";
+import { FieldValues, useForm } from "react-hook-form";
 
 const AuthForm = () => {
+  const [formType, setFormType] = useState<FORM_TYPE>("Login");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FieldValues>({
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const toggleFormType = useCallback(() => {
+    setFormType(formType === "Login" ? "Register" : "Register");
+  }, [formType]);
+
   return <div>AuthForm</div>;
 };
 
---
+++ app/components/Auth/types.ts
@@ -0,0 +1,1 @@
+export type FORM_TYPE = "Login" | "Register";

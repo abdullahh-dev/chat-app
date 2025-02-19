b9ccee2e771b9fcb161c17ab837dd089b4a0b9af
abdullahh-dev <abdullahaha101@example.com>
2025-02-19 11:12:34 +0500

    modified project structure

---
+++ app/(site)/page.tsx
@@ -0,0 +1,18 @@
+import AuthForm from "../components/Auth/AuthForm";
+
+export default function Home() {
+  return (
+    <section className="flex flex-col justify-center bg-primary py-12 sm:px-6 lg:px-8 min-h-full">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2
+          className="mt-6 text-center text-3xl
+    tracking-tight
+    text-gray-200"
+        >
+          Sign in to your account
+        </h2>
+      </div>
+      <AuthForm />
+    </section>
+  );
+}
